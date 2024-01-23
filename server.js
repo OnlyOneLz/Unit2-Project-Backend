bc7d030b9e3abe796a8fb088a9b04e1410c6ede4
OnlyOneLz <elliotrobertlewis@hotmail.com>
2024-01-23 15:41:54 +0000

    router update

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import serverless from "serverless-http";
-import { Router as router} from "express";
 
 const app = express();
 
